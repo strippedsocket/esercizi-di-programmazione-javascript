1a3e4c30131ad45d0cf78e336c4478f89ed51d00
Carlotta Concina <stripped.socket@gmail.com>
2016-11-17 19:26:12 +0100

    risoluzione conta-il-tempo

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -6,4 +6,11 @@
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
-var time=0;
+var time = 12700;
+var hours = Math.floor(time/3600);
+var hresto = time%3600;
+console.log(hresto);
+var min = Math.floor(hresto/60);
+var minresto = hresto%60;
+var sec = minresto;
+console.log(hours, min, sec);
